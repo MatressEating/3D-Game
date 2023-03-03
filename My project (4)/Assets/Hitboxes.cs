5186ae24b3101d2617c770c995dff140f2e7be94
MatressEating <118852588+MatressEating@users.noreply.github.com>
2023-03-03 08:44:39 -0800

    working on hitboxes

---
+++ My project (4)/Assets/Hitboxes.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitboxes : MonoBehaviour
+{
+
+    public Collider[] hitbox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        { 
+            LaunchAttack(hitbox[0]);
+            Debug.Log("G pressed");
+        }
+    }
+
+    private void LaunchAttack(Collider col)
+    {
+        Collider[] cols = Physics.OverlapBox(col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask("Hitbox"));
+        foreach (Collider c in cols)
+            Debug.Log(c.name);
+
+    }
+}
